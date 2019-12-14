df56d4b6144d85900b2828d75ee584361d8f840a
Dima <katri4dimon@i.ua>
2019-12-14 11:57:37 +0200

    fixed problem in project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import React from 'react'
 
-const HelloWorld = () => {
-	return (
-		<h1>Hello world</h1>
-	)
-}
-
+import './reset.css'
+import './base.css'
+import './header.css'
+import './menu.css'
+import './cart.css'
 
 const App = () => {
 	return (
 		<div>
-			<HelloWorld/>
-			<p>Qwerty</p>
-		</div>
-)
-}
+			<header className="header">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-3">
+							<div className="logo">
+								<img src="img/logo.png" alt=""/>
+							</div>
+						</div>
+						<div className="col-lg-6">
+							<div className="menu">
+								<ul>
+									<li><a href="">Home</a></li>
+									<li><a href="">Products</a></li>
+									<li><a href="">Payment</a></li>
+									<li><a href="">Shipping</a></li>
+								</ul>
+							</div>
+						</div>
+						<div className="col-lg-3">
+							<div className="cart text-center">
+								<div className="products-count">0</div>
+								<div className="products-price">$500</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			<main className="main">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-3">
+							
+						</div>
+						<div className="col-lg-9">
+							
+						</div>
+					</div>
+				</div>
+			</main>
+			<footer className="footer"></footer>
+				</div>
+	)
+			}
 
 export default App
