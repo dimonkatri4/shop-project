b3973d54a3e70b2bfc8f991b3dde111c08d43581
Dima <katri4dimon@i.ua>
2019-12-15 12:15:55 +0200

    create productsData masyv

---
+++ src/App/Main/Products/productsData.js
@@ -0,0 +1,32 @@
+const productsData = [
+    {
+        name:"Iphone X",
+        description:"This is Iphone X",
+        type:"Phone",
+        capacity:"256",
+        price:"1000",
+    },
+    {
+        name:"Iphone XS",
+        description: "This is Iphone XS",
+        type: "Phone",
+        capacity: "64",
+        price: "1700",
+    },
+    {
+        name:"Iphone XS Max",
+        description : "This is Iphone XS MAx",
+        type: "Phone",
+        capacity : "128",
+        price: "900",
+    },
+    {
+        name: "Iphone 7 Plus",
+        description: "This is Iphone 7 Plus",
+        type: "Phone",
+        capacity: "128",
+        price: "740",
+    }
+]
+
+export default productsData
