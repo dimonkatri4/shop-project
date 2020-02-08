b06168caac4e55415376e31800f7cf60b4bd2c9e
Dima <katri4dimon@i.ua>
2020-02-08 13:21:14 +0200

    end lesson

--- src/Components/Cart/CartProductList.js
+++ src/Components/Cart/CartProductList.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import productsData, { getProductsMap } from '../../App/Main/Products/productsData'
 import {keys} from 'lodash'
+import CartProductListItem from './CartProductListItem'
 
 const CartProductList = ({
     productInCart,
-    productsMap = getProductsMap(productsData)
+    productsMap = getProductsMap(productsData),
+    CartList = CartProductListItem
     }) => {
     return (
         keys(productInCart).map((productId)=>(
-            <div key={productId}>
-                 {productsMap[productId].name}: {productInCart[productId]}
-            </div>
+            <CartList
+                product = {productsMap[productId]}
+                productCount = {productInCart[productId]}
+            />
         )) 
     )
 }
