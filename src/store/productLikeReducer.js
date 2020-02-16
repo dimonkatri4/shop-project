8c9f5413613dcef6777b3e8f7b563aca4187c9e6
Dima <katri4dimon@i.ua>
2020-02-16 12:18:49 +0200

    добавили в редюсер дію - action

--- src/store/productLikeReducer.js
+++ src/store/productLikeReducer.js
@@ -1,3 +1,20 @@
 
 
-export const productLikeReducer = (state = {1:true,2:true},action) => state
+export const productLikeReducer = (state = {1:true,2:true},action) => {
+    switch(action.type) {
+
+        case "LIKE":
+            return {
+                ...state,
+                [action.id]:true
+            }
+
+            case "DISLIKE":
+            return {
+                ...state,
+                [action.id]:false
+            }
+            default:
+                return state
+    }
+}
